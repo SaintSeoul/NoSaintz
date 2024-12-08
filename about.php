d7e132dc382fb6f00ed2b637a55e66a799f9f2ea
saint <alexandrievmigallos@gmail.com>
2024-12-08 23:56:40 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,199 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/sk8.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Team message:</h3>
+         <p>To All Our NoSaintz Supporters,
+
+We’ve been grinding, flipping, and rolling because of YOU—the heartbeat of our NoSaintz family. From the streets to the skateparks, your loyalty fuels our passion to keep pushing boundaries and creating dope designs for the culture we all love.
+
+We’re hyped to announce the NoSaintz Skate Team—a squad repping creativity, grit, and raw street energy. This crew embodies what we stand for: staying true, supporting each other, and dominating the underground scene.
+
+Big love to every skater, supporter, and dreamer rocking with us. You’re not just customers; you’re the real legends of this movement. Keep riding with us as we take NoSaintz to the next level!
+
+Stay True. Stay NoSaintz.
+🖤 Team NoSaintz</p>
+
+         <p>I would like to thank <a href="https://www.facebook.com/er.ashokbasnet" target="_blank">our manager Sir Nekz </a> Sir for guiding me through the session and making me able to develop projects like this. once again this 
+         Smkyyy,Saint,Seoul and Ajy </p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/kyo.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Online e-products working great as well. Only issue I have is they usually deliver when I'm a little caught up, though I've set a preferred delivery time. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> <a href="https://www.facebook.com/profile.php?id=100083292714419" target="_blank">Kyonosuke Yamashita</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/hell.jpg" alt="">
+         <p>It is the first online services in Seoul which we can trust completely.I always unbox making a video and instantly complain if there's anything wrong. Sometimes even don't need to return the item and they process the refund. nosaintz do heavy fine to sellers who send wrong products thats why its platform getting better day by day.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/profile.php?id=100075602340579" target="_blank">HellMerry</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/wiz.jpg" alt="">
+         <p>nosaintz is great if you choose good sellers . A variety of required item available . Customers can return and refund full amount within 7 days easily . Nosaintz is boosting eCommerce business in Kathmandu.It provides great opportunity to sale items online with ease.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/kaushalsah135790" target="_blank">Wiz Khalifa</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/jen.jpg" alt="">
+         <p>Using Nosaintz for online shopping from almost 3 years. Outstanding experience with them. Game vouchers and pick up point as delivery with 0 shipping charges are super saving services.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/fuccheekta.moh.1" target="_blank">JennieRubyJane</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/noze.jpg" alt="">
+         <p>I have been using their services for the last 2 years and I have found them extremely reliable.Their return policy is what gives you an extra layer of reliance and peace of mind. In case the product doesn't meet your expectations or if there is any fault in it. then you can return the product within seven days from the date of delivery.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/ranjitchaudhary159" target="_blank">NozeWorld</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/K.jpg" alt="">
+         <p>NoSaintz is cool! I have ordered hundreds of products from it and never got any scam. It delivers products in time with out delay. Packaging of products are strong and delivery rates are too low. Just amazing Website will keep shopping from KinBech.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/pra.x.nil"  target="_blank">KatarinaBlueee</a></h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
